c13f7e61da80f7a9c2bbfb9461575577523338e8
Manoj kumar <manoj166174@gmail.com>
2025-06-01 23:17:05 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,13 @@
+// Java Program to Illustrate concat() method of String.
+class GFG {
+ 
+    public static void main(String args[]) {
+      
+        // String Initialization
+        String s = "manoj";
+       
+        // Use concat() method for string concatenation
+        s = s.concat("kumar");
+        System.out.println(s);
+    }
+}
